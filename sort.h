987a1f3508da575d74db66966d3ee4837b37ee44
jswini <jswinikoff@gmail.com>
2022-05-05 15:21:35 -0500

    first commit sort.h

---
+++ sort.h
@@ -0,0 +1,25 @@
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
+
+#ifndef SORT_H
+#define SORT_H
+#include <stdio.h>
+#include <stdlib.h>
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void merge_sort(int *array, size_t size);
+int  copy_array(int *array, int start_index, int end_index, int *array_copy);
+void top_down_split(int *array2, int start_index, int end_index, int *array3);
+void top_down(int *array, int start_index, int center_index, int end_index, int *array2);
+#endif /*SORT_H*/
